4b29fda65ea4ef29d73a0691ef9c9dba44feff1a
Chris Pirich <5429146+cpirich@users.noreply.github.com>
2024-01-05 14:06:51 -0800

    Initial commit from Create Figma Plugin

---
+++ src/ui.tsx
@@ -0,0 +1,26 @@
+import { render, useWindowResize } from '@create-figma-plugin/ui'
+import { h } from 'preact'
+import '!./output.css'
+import { emit } from '@create-figma-plugin/utilities'
+
+import { ResizeWindowHandler } from './types'
+
+function Plugin() {
+  function onWindowResize(windowSize: { width: number; height: number }) {
+    emit<ResizeWindowHandler>('RESIZE_WINDOW', windowSize)
+  }
+  useWindowResize(onWindowResize, {
+    maxHeight: 320,
+    maxWidth: 320,
+    minHeight: 120,
+    minWidth: 120,
+    resizeBehaviorOnDoubleClick: 'minimize'
+  })
+  return (
+    <h1 class="text-3xl font-bold underline">
+      Hello, World!
+    </h1>
+  )
+}
+
+export default render(Plugin)
