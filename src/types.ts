8a94fa8f47db4848a1774b73b4eb02eb5b67a959
Chris Pirich <5429146+cpirich@users.noreply.github.com>
2024-01-08 16:38:05 -0800

    Add eslint, prettier

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,6 @@
-import { EventHandler } from '@create-figma-plugin/utilities'
+import { EventHandler } from "@create-figma-plugin/utilities";
 
 export interface ResizeWindowHandler extends EventHandler {
-  name: 'RESIZE_WINDOW'
-  handler: (windowSize: { width: number; height: number }) => void
+  name: "RESIZE_WINDOW";
+  handler: (windowSize: { width: number; height: number }) => void;
 }
--- src/ui.tsx
+++ src/ui.tsx
@@ -1,26 +1,23 @@
-import { render, useWindowResize } from '@create-figma-plugin/ui'
-import { h } from 'preact'
-import '!./output.css'
-import { emit } from '@create-figma-plugin/utilities'
+import { render, useWindowResize } from "@create-figma-plugin/ui";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { h } from "preact";
+import "!./output.css";
+import { emit } from "@create-figma-plugin/utilities";
 
-import { ResizeWindowHandler } from './types'
+import { ResizeWindowHandler } from "./types";
 
 function Plugin() {
   function onWindowResize(windowSize: { width: number; height: number }) {
-    emit<ResizeWindowHandler>('RESIZE_WINDOW', windowSize)
+    emit<ResizeWindowHandler>("RESIZE_WINDOW", windowSize);
   }
   useWindowResize(onWindowResize, {
     maxHeight: 320,
     maxWidth: 320,
     minHeight: 120,
     minWidth: 120,
-    resizeBehaviorOnDoubleClick: 'minimize'
-  })
-  return (
-    <h1 class="text-3xl font-bold underline">
-      Hello, World!
-    </h1>
-  )
+    resizeBehaviorOnDoubleClick: "minimize",
+  });
+  return <h1 class="text-3xl font-bold underline">Hello, World!</h1>;
 }
 
-export default render(Plugin)
+export default render(Plugin);
